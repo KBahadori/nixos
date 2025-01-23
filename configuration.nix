0d0ce461cad36f4f8062ad50aad6551a30f90903
KBahadori <89547458+KBahadori@users.noreply.github.com>
2025-01-23 00:39:09 +0000

    Update configuration.nix
    updated

--- configuration.nix
+++ configuration.nix
@@ -1,75 +1,53 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, inputs, ... }:
-
-
 {
-  disabledModules = [ "virtualisation/virtualbox-host.nix" ];
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./nvidia.nix
-      ./plasma.nix
-      ./hardware-acceleration.nix
-       <nixos-hardware/omen/14-fb0798ng>
-      ./modules/virtualbox-host.nix
-      #./gnome.nix 
 
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./nvidia.nix
+    ./plasma.nix
+    ./hardware-acceleration.nix
+  ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.efiSupport = true; 
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
+  # Firmware
+  hardware.enableAllFirmware = true;
+  hardware.cpu.intel.updateMicrocode = true;
 
-#  filesystems
-#  services.xserver.enable = true;
-#  services.xserver.displayManager.gdm.enable = true;
-#  services.xserver.desktopManager.gnome.enable = true;
+  # Bootloader (using GRUB)
+  boot = {
+    loader = {
+      systemd-boot.enable = false;
+      grub.enable = true;
+      grub.device = "nodev";
+      grub.useOSProber = false;
+      grub.efiSupport = true;
+      grub.font = "/boot/grub/fonts/unicode.pf2";
+    };
+    loader.efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
 
-  # additional boot params for wayland
-  boot.kernelParams = [ "nvidia-drm.modeset=1" "snd_intel_dspcfg.dsp_driver=1" "snd_hda_intel.dmic_detect=0" ];
+    # Use latest kernel with NVIDIA
+    kernelPackages = pkgs.linuxPackages_latest;
+    blacklistedKernelModules = [ "nouveau" ];
+   # kernelParams = [ "nvidia-drm.modeset=1" ];
+    initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
 
-  boot.blacklistedKernelModules = [ "nouveau" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  
-  networking.hostName = "nixos"; # Define your hostname.
- # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # Additional modprobe config
+    extraModprobeConfig = lib.mkDefault ''
+      options snd slots=snd_usb_audio,snd-hda-intel
+    '';
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  
- # networking.networkmanager.wifi = {
- #  ssid = "eduroam";
- #  mode = "client";
- #  authentication = {
- #   eap = "peap";
- #   ca-cert = "/home/kb759/.config/cat_installer/ca.pem";
- #   identity = "kb759+laptop2@cam.ac.uk";
- #   anonymous-identity = "_token-public@cam.ac.uk";
- #   password = "jpbs46bmt73xgyhr";
- #   phase2-auth = "mschapv2";
- #   };
- # };
-  
-  boot.initrd.kernelModules = 
-  [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
-  # Enable networking
-  networking.networkmanager.enable = true;
-  
-  # Set your time zone.
+  # Networking
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
+
+  # Time & Locale
   time.timeZone = "Europe/London";
- 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_GB.UTF-8";
     LC_IDENTIFICATION = "en_GB.UTF-8";
@@ -82,83 +60,73 @@
     LC_TIME = "en_GB.UTF-8";
   };
 
-  # Configure keymap in X11
+  # Keyboard settings
   services.xserver.xkb = {
     layout = "gb";
     variant = "";
   };
-
-  # Configure console keymap
   console.keyMap = "uk";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # intel CPU
-  hardware.cpu.intel.updateMicrocode = true;
-  
-
-  # Enable sound with pipewire.
+  # PipeWire (instead of PulseAudio)
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
-  enable = true;
-  alsa.enable = true;
-  alsa.support32Bit = true;
-  pulse.enable = true;
-  # If you want to use JACK applications, uncomment this
-  jack.enable = true;
-  audio.enable = true;
-   # use the example session manager (no others are packaged yet so this is enabled by default,
-  # no need to redefine it in your config for now)
-  #services.pipewire.wireplumber = true;
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-  
-   # Vulkan support
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  # Environment variables for NVIDIA/Wayland
   environment.variables = {
-  VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json";
-  VK_LAYER_PATH = "/run/opengl-driver/share/vulkan/implicit_layer.d";
-  LIBVA_DRIVER_NAME = "nvidia";
-  GBM_BACKEND = "nvidia-drm";
-  __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json";
+    VK_LAYER_PATH = "/run/opengl-driver/share/vulkan/implicit_layer.d";
+    LIBVA_DRIVER_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    ELECTRON_ENABLE_WAYLAND = "1";
+    ELECTRON_USE_GL = "egl";
+    ELECTRON_ENABLE_VAAPI = "1";
+    OZONE_PLATFORM = "wayland";
+    ELECTRON_FLAGS = "--enable-features=VaapiVideoDecoder --use-gl=egl --enable-hardware-overlays --ozone-platform=wayland";
   };
 
-  # Define GPU and electron use
-  environment.variables = {
-  ELECTRON_ENABLE_WAYLAND = "1";   # Enables Wayland for Electron apps
-  ELECTRON_USE_GL = "egl";        # Forces EGL for rendering
-  ELECTRON_ENABLE_VAAPI = "1";    # Enables VA-API for video acceleration
-  OZONE_PLATFORM = "wayland";     # Specifies Wayland platform
-  #CUDA_VISIBLE_DEVICES = "1";     # Prioritize the RTX 3080 (GPU index)
- # NIXOS_OZONE_WL = "1"; 
-  };
-  environment.variables.ELECTRON_FLAGS = "--enable-features=VaapiVideoDecoder --use-gl=egl --enable-hardware-overlays --ozone-platform=wayland";
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User
   users.users.kb759 = {
     isNormalUser = true;
     description = "kb759";
-    extraGroups = [ "networkmanager" "wheel" "audio" "vboxusers" ];
+    extraGroups = [ "networkmanager" "wheel" "audio" "vboxusers" "fuse" ];
     packages = with pkgs; [
       kdePackages.kate
       thunderbird
     ];
     shell = pkgs.fish;
   };
-  
-  # hyprland config
+
+  # Hyprland + XWayland
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
+  # Cosmic IDE
+  services.desktopManager.cosmic.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
+  services.flatpak.enable = true;
+
+  # PolicyKit
   security.polkit.enable = true;
 
+  # Fonts
   fonts.packages = with pkgs; [
     noto-fonts
-    noto-fonts-cjk
+    noto-fonts-cjk-sans
     noto-fonts-emoji
     liberation_ttf
     fira-code
@@ -167,128 +135,93 @@
     dina-font
     proggyfonts
   ];
-  services.displayManager.sddm.wayland.enable = true;
-  
 
-  # Install firefox.
-  programs.firefox.enable = true;
+  # SDDM (Wayland)
+  services.displayManager.sddm.wayland.enable = true;
+
+  # X server (often needed for Plasma, certain apps, etc.)
   services.xserver.enable = true;
- # services.xserver.displayManager.enable = true;
- # services.xserver.desktopManager.gnome.enable = true;
-  
-  # Steam
-  programs.steam = {
-    enable = true;
-    #remotePlay.openFirewall = true;
-    #dedicatedServer.openFirewall = true;
-    gamescopeSession.enable = false;
-    };
-  
-  programs.gamemode.enable = true; 
-#  xdg.portal.enable = true;
-#  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  # Browser, Steam, etc.
+  programs.firefox.enable = true;
+  programs.steam.enable = true;
+  programs.gamemode.enable = true;
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # System packages
   environment.systemPackages = with pkgs; [
-   kitty
-   brave
-   helix 
-   hyprland
-   wayland
-   wayland-utils
-   wl-clipboard
-   xwayland  # Required for compatibility with X11 apps
-   kitty     # Optional: Wayland-compatible terminal emulator
-   mako      # Optional: Notification daemon
-   fish
-   qt6.qtwayland
-   libsForQt5.qt5.qtwayland
-   kdePackages.plasma-workspace
-   kdePackages.plasma-wayland-protocols
-   jetbrains.pycharm-professional
-   vesktop
-   vscode
-   ranger
-   mangohud
-   protonup
-   mesa
-   spotify
-   discord
-   mesa-demos
-   virtualbox
-   dconf
-   xdg-desktop-portal-hyprland
-   wofi
-   R
-   python3
-   waybar
-   intel-ocl
-   pipewire
-   pavucontrol  # For GUI audio control
-   sof-firmware
-   alsa-ucm-conf
-   lshw
-   wireplumber
-   wpa_supplicant
-   networkmanager
-   dbus
-   rclone
-   joplin
-   joplin-desktop
-   libsForQt5.qtstyleplugin-kvantum
-   ];
+    kitty
+    brave
+    helix
+    #hyprland
+    wayland
+    wayland-utils
+    wl-clipboard
+    xwayland     # for X apps on Wayland
+    mako         # notification daemon
+    fish
+    qt6.qtwayland
+    libsForQt5.qt5.qtwayland
+    kdePackages.plasma-workspace
+    kdePackages.plasma-wayland-protocols
+    jetbrains.pycharm-professional
+    vesktop
+    vscode
+    ranger
+    mangohud
+    protonup
+    mesa
+    spotify
+    discord
+    mesa-demos
+    virtualbox
+    dconf
+    xdg-desktop-portal-hyprland
+    wofi
+    R
+    python3
+    waybar
+    intel-ocl
+    pipewire
+    pavucontrol
+    sof-firmware
+    alsa-ucm-conf
+    lshw
+    wireplumber
+    wpa_supplicant
+    networkmanager
+    dbus
+    rclone
+    joplin
+    joplin-desktop
+    libsForQt5.qtstyleplugin-kvantum
+    vulkan-tools
+    vulkan-loader
+    grim
+    swappy
+    slurp
+    git
+    btop
+    htop
+    libreoffice-qt6-fresh
+    sshfs
+  ];
 
-    programs.fish = {
-      enable = true;
-       };
-    
- 
-  # Version
-  system.stateVersion = "24.11";
- # nixpkgs.config.allowUnfree = pkgs.lib.mkForce = true;
- # services.xserver.videoDrivers = lib.mkDefault ["nvidia"];
- # environment.sessionVariables = {
-  #  STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/user/.steam/root/compatibilitytools.d";
-   # };  
-  
-  
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Fish shell
+  programs.fish.enable = true;
 
-  # List services that you want to enable:
-  #virtualisation.virtualbox.host.enableKvm = true;
-  users.groups.vboxusers = { };
-  users.extraGroups.vboxusers.members = [ "kb759"];
-  #virtualisation.virtualbox.host.enableExtensionPack = true;
-  services.udev.extraRules = ''
-  SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", GROUP="vboxusers", MODE="0660"
-'';
+  # VirtualBox groups
+  users.groups.vboxusers = {};
+  users.extraGroups.vboxusers.members = [ "kb759" ];
 
-
-  # Enable the OpenSSH daemon.
+  # Enable OpenSSH
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # NixOS state version
+  system.stateVersion = "22.11";
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html  system.stateVersion = "24.05"; # Did you read the comment?
-
-  # enable flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes"]; 
+  # Enable Nix flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
